69f76980523b20ac315ad58b84ee74d17f254fef
Alejandro Chacón <alexchaconvargas@gmail.com>
2016-07-19 15:06:02 +0200

    new files

--- app/pages/events/eventItem.ts
+++ app/pages/events/eventItem.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-
+import { Event } from './EventModel';
 @Component({
 	selector: 'event-item',
   templateUrl: 'build/pages/events/eventItem.html'
 })
 
 export class EventItem {
-	constructor() {
+	evento: Event;
+	constructor(){
 	}
-
 };
--- app/pages/events/events.ts
+++ app/pages/events/events.ts
@@ -2,7 +2,14 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import * as moment from 'moment';
 import {EventItem} from './eventItem';
+import { Event } from './EventModel';
 //import {findAll} from '../../services/eventsService';
+
+const EVENTS : Event[] = [
+	{ id: 1, name: 'Swing Ballroom' , description: 'aaa', place: 'almo2bar', addr: 'C/ bruniquer 50'},
+  	{ id: 2, name: 'Jam de swingmaniacs', description: 'bbb', place: 'SwingManiacs', addr : 'C/ Esglesia 12'}
+]
+
 @Component({
   templateUrl: 'build/pages/events/events.html',
   directives: [EventItem]
@@ -12,10 +19,13 @@ export class Events {
 	today: string;
 	days: string[];
 	events : Array<Event>;
+	title = "Eventos";
+	eventos = EVENTS;
 
 	constructor(private navController: NavController) {
 		this.today = moment().format("DD/MM");
 		this.days = [];
+
 		//Tomorrow
 		this.days.push(moment().add(1, 'days').format("DD/MM"));
 		this.days.push(moment().add(2, 'days').format("DD/MM"));
